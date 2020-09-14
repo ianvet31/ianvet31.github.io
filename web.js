c6abb52636ff7074a4c382a153c9a8cf290bd5b6
ian vetter <61299105+ianvet31@users.noreply.github.com>
2020-09-13 20:35:38 -0500

    Update web.js

--- web.js
+++ web.js
@@ -7,17 +7,18 @@ var pics = [
 	"imgs/e6.jpg"
 ];
 
+var button = document.querySelector("button");
 var img = document.querySelector("img");
 var counter = 1;
 
-function elemphant(btn) {
-	if(counter === 6){
+
+button.addEventListener("click", function() {
+	if(counter == 6){
 		counter = 0;
 	}
-	img.src = pics[counter]
+	img.src = pics[counter];
 	counter = counter + 1;
-
-}
+});
 
 function playPause(btn,vid) {
 	var vid = document.getElementById(vid);
