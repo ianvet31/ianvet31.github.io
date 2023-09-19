3e6697f3c2bdaf61d9c0d4fec30c180497660a9e
ivette2 <ivette2@illinois.edu>
2023-09-18 20:26:59 -0500

    Added features

--- aboutjs.js
+++ aboutjs.js
@@ -1,19 +1,15 @@
-var pics = [
-	"imgs/e1.jpg",
-	"imgs/e2.jpg",
-	"imgs/e3.jpg",
-	"imgs/e4.jpg",
-	"imgs/e5.jpg",
-	"imgs/e6.jpg"
+var pics = ["imgs/e1.jpg","imgs/e2.jpg","imgs/e3.jpg","imgs/e4.jpg","imgs/e5.jpg","imgs/e6.jpg",
+"imgs/e7.jpg","imgs/e8.jpg","imgs/e9.jpg","imgs/e10.jpg","imgs/e11.jpg","imgs/e12.jpg","imgs/e13.jpg",
+"imgs/e14.jpg","imgs/e15.jpg","imgs/e16.jpg","imgs/e17.jpg","imgs/e18.jpg","imgs/e19.jpg","imgs/e20.jpg"
 ];
 
-var btn = document.querySelector("button");
-var img = document.querySelector("img");
+var btn = document.getElementById("e_button");
+var img = document.getElementById("imger");
 var counter = 1;
 
 btn.addEventListener("click", function(){
-	if(counter === 6){
-		counter = 0;)
+	if(counter === 20){
+		counter = 0;
 	}
 	img.src = pics[counter]
 	counter = counter + 1;
